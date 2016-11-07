881ede388b0e7ca2a4344090174a5501f4148c22
Tran Hoang Long <hoanglongckd@gmail.com>
2016-11-07 09:10:38 +0700

    add login service

--- src/controller/AppService.java
+++ src/controller/AppService.java
@@ -2,11 +2,14 @@ package controller;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -36,6 +39,19 @@ public class AppService {
 
 		return taBo.getListTable();
 
+	}
+	
+	@POST
+	@Path("/login")
+	@Produces(MediaType.APPLICATION_XML)
+	@Consumes(MediaType.APPLICATION_XML)
+	public void login(@FormParam("username") String username,
+		      @FormParam("password") String password,
+		      @Context HttpServletResponse servletResponse) {
+		System.out.println(username);
+		System.out.println(password);
+//		String output = "POST:Jersey say : " + objCat.getName();
+//		return Response.status(200).entity(output).build();
 	}
 
 //	@POST
